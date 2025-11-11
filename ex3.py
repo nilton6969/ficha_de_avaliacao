d36d68e8abfef30a3d5ee9a97d17a191a7997c96
Nilton <aluno24823@mtorga.edu.pt>
2025-11-11 20:23:13 +0000

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,14 @@
+extenso = int(input("Digite um  número de 1 á 5: "))
+
+if extenso == 1 :
+    print("um")
+elif extenso == 2 :
+    print("Dois")
+elif extenso == 3 :
+    print("Três")    
+elif extenso == 4 :
+    print("Quatro")
+elif extenso == 5 :
+    print("Cinco")
+else :
+    print("Número inválido")
