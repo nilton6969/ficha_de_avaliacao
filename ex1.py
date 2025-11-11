5c8e35a7ec770a99f3a72ee6c4acebde7c592167
Nilton <aluno24823@mtorga.edu.pt>
2025-11-11 20:21:13 +0000

    Add ex1.py to the repository

---
+++ ex1.py
@@ -0,0 +1,16 @@
+dia = int(input("digite um numero de 1 a 7"))
+
+if dia == 1 :
+    print("segunda-feira")
+elif dia == 2 :
+    print("Terça-feira")
+elif dia == 3 :
+    print("Quarta-feira")
+elif dia == 4 :
+    print ("Quinta-feira")
+elif dia == 5 :
+    print("Sexta-feira")
+elif dia == 6 :
+    print("Sábado")
+else :
+    print("Domigo")
