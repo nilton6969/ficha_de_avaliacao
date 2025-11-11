6a207212412eb5d8e744be566ac10e7b0f64753d
Nilton <aluno24823@mtorga.edu.pt>
2025-11-11 20:23:48 +0000

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,12 @@
+idade = int(input("Diga-me a tua idade para que possa atribuir uma categoria :  "))
+
+if idade >5 and idade <=7:
+     print("A tua categoria é a Infantil A ")
+elif idade >8 and idade <=11:
+     print("A tua categoria é a Infantil B ")
+elif idade >12 and idade <=13:
+     print("A tua categoria é a Juvenil A ")
+elif idade >14 and idade <=17:
+     print("A tua categoria é a Juvenil B ")
+else:
+     print("A tua categoria é a Adulto ")
